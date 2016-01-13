02498931d8754234537d7ecf1013287e0c0b767f
Brian Hyder <brianhyder@gmail.com>
2016-01-13 13:26:20 -0500

    Updated for 0.5.0

--- news-sitemap.js
+++ news-sitemap.js
@@ -16,7 +16,7 @@
 */
 
 module.exports = function(pb) {
-    
+
     /**
     * NewsSitemap - Adds a Google News sitemap to the site.
     *
@@ -26,46 +26,54 @@ module.exports = function(pb) {
     function NewsSitemap(){}
 
     /**
-    * Called when the application is being installed for the first time.
-    *
-    * @param cb A callback that must be called upon completion.  cb(err, result).
-    * The result is ignored
-    */
-    NewsSitemap.onInstall = function(cb) {
+     * Called when the application is being installed for the first time.
+     * @static
+     * @method onInstallWithContext
+     * @param {Object} context
+     * @param {Object} context.site
+     * @param {Function} cb A callback that must be called upon completion.  cb(Error, Boolean).
+     * The result should be TRUE on success and FALSE on failure
+     */
+    NewsSitemap.onInstallWithContext  = function(context, cb) {
         cb(null, true);
     };
 
     /**
-    * Called when the application is uninstalling this plugin.  The plugin should
-    * make every effort to clean up any plugin-specific DB items or any in function
-    * overrides it makes.
-    *
-    * @param cb A callback that must be called upon completion.  cb(err, result).
-    * The result is ignored
-    */
-    NewsSitemap.onUninstall = function(cb) {
+     * Called when the application is uninstalling this plugin.  The plugin should
+     * make every effort to clean up any plugin-specific DB items or any in function
+     * overrides it makes.
+     * @static
+     * @method onUninstallWithContext
+     * @param {Object} context
+     * @param {Object} context.site
+     * @param {Function} cb A callback that must be called upon completion.  cb(Error, Boolean).
+     * The result should be TRUE on success and FALSE on failure
+     */
+    NewsSitemap.onUninstallWithContext = function(context, cb) {
         cb(null, true);
     };
 
     /**
-    * Called when the application is starting up. The function is also called at
-    * the end of a successful install. It is guaranteed that all core PB services
-    * will be available including access to the core DB.
-    *
-    * @param cb A callback that must be called upon completion.  cb(err, result).
-    * The result is ignored
-    */
-    NewsSitemap.onStartup = function(cb) {
+     * Called when the application is starting up. The function is also called at
+     * the end of a successful install. It is guaranteed that all core PB services
+     * will be available including access to the core DB.
+     *
+     * @param context
+     * @param cb A callback that must be called upon completion.  cb(Error, Boolean).
+     * The result should be TRUE on success and FALSE on failure
+     */
+    NewsSitemap.onStartupWithContext = function (context, cb) {
         cb(null, true);
     };
 
     /**
-    * Called when the application is gracefully shutting down.  No guarantees are
-    * provided for how much time will be provided the plugin to shut down.
-    *
-    * @param cb A callback that must be called upon completion.  cb(err, result).
-    * The result is ignored
-    */
+     * Called when the application is gracefully shutting down.  No guarantees are
+     * provided for how much time will be provided the plugin to shut down.
+     * @static
+     * @method onShutdown
+     * @param {Function} cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
     NewsSitemap.onShutdown = function(cb) {
         cb(null, true);
     };
